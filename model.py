acb04cffaa0563e7afa46deb2344873744720c13
Sam <@kensho.com>
2014-07-18 02:33:24 -0400

    piping works, but is slow

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import numpy as np
 #import statsmodels.api as sm
-#from itertools import combinations
+from itertools import combinations
 from sklearn_pandas import DataFrameMapper, cross_val_score
 from scrapeDX import make_colpro
 from sklearn import preprocessing
@@ -16,13 +16,12 @@ def df_mapper(lh):
         colpro[col] = colpro[col].astype('float')
         to_map.append((col,sklearn.preprocessing.StandardScaler()))
     mapper = DataFrameMapper(to_map)
-    return mapper.fit_transform(colpro)
+    return mapper
 
 def piping(colpro, mapper):
     pipe = sklearn.pipeline.Pipeline([('featurize', mapper), ('lm', sklearn.linear_model.LinearRegression())])
     x = cross_val_score(pipe,colpro, colpro.off100, 'r2',verbose=2)
-    return x
-
+    return np.mean(x)
 
 def genetic_loop(colpro, num_lh, stop=1000000000000):
     ivs = lh_vars(colpro)
@@ -32,12 +31,17 @@ def genetic_loop(colpro, num_lh, stop=1000000000000):
     #best_result = reg
     for i, combo in enumerate(combinations(ivs,num_lh)):
         lh = list(combo)
-        result = regress(colpro[lh], rh)
+        mapper = df_mapper(lh)
+        result = piping(colpro, mapper)
         if i == 0:
             best_result = result
-            continue
-        if result.rsquared_adj > best_result.rsquared_adj:
+        elif result.rsquared_adj > best_result.rsquared_adj:
             best_result = result
         if i == stop:
             break
     return result #Can return best_vars if useful
+
+
+def regress(X, Y): # FASTER THAN PIPING, CHANGE TO RESULT>rsquared_adj
+    results = sm.OLS(Y,X)
+    results = result
