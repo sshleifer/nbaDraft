252b6e136ebb22fe8271654e6699da3204a13b3f
Sam <@kensho.com>
2014-07-19 16:13:37 -0400

    factored out modeling

--- model.py
+++ model.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import requests
 import numpy as np
-#import statsmodels.api as sm
+import statsmodels.api as sm
 from itertools import combinations
 from sklearn_pandas import DataFrameMapper, cross_val_score
-from scrapeDX import make_colpro
 from sklearn import preprocessing
-from scrapeDX import make_colpro,lh_vars
+from assemble_dataset import make_colpro,lh_vars
 import sklearn
 
 def df_mapper(lh):
@@ -43,5 +42,20 @@ def genetic_loop(colpro, num_lh, stop=1000000000000):
 
 
 def regress(X, Y): # FASTER THAN PIPING, CHANGE TO RESULT>rsquared_adj
-    results = sm.OLS(Y,X)
-    results = result
+    reg = sm.OLS(Y,X)
+    return reg.fit()
+
+def full_reg(colpro):
+    reg = regress(colpro[lh_vars()],colpro['off100'])
+    #genetic_loop(colpro[lh_vars()],colpro['off100'])
+    return reg
+
+
+def best_num_vars(colpro):
+    results = {}
+    print len (colpro.columns)
+    for i in range(1, len(colpro.columns)):
+        result = genetic_loop(colpro, i, 5000)
+        results.update({len(result.pvalues): result.rsquared_adj})
+        print i, results[i]
+    return results
