1742861c51f752b9f4b7b2f244d05389f89676c9
Sam <@kensho.com>
2014-08-02 23:06:50 -0400

    to avoid url garbage when reading code

---
+++ constants.py
@@ -0,0 +1,5 @@
+MEAS_URL = 'http://www.draftexpress.com/nba-pre-draft-measurements/?page=&year=All&source=All&sort2=ASC&draft=0&pos=0&sort='
+RAPM_URL = 'http://stats-for-the-nba.appspot.com/ratings/'
+DX_YEARS = ['02', '03', '04', '05', '06', '07',
+       '08', '09', '10', '11', '12', '13', '14']
+MOCK_URL = 'http://www.draftexpress.com/nba-mock-draft/'
