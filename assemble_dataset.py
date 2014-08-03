a4c7baf61ec417dbfaf0519e490b80a47febce5a
Sam <@kensho.com>
2014-08-03 00:38:29 -0400

    did some linting

--- assemble_dataset.py
+++ assemble_dataset.py
@@ -1,70 +1,64 @@
+'''Assembles and preprocesses the colPro.csv, the dataset used for analysis'''
 import numpy as np
 import pandas as pd
+MEAS_PATH = 'datasets/measurements.csv'
+COL_PATH = 'datasets/colData.csv'
 
-def lh_vars(colpro, col_path = 'colData.csv', meas_path = 'measurements.csv'): #TODO: CACHE
-    col = drop_unnamed(pd.read_csv(col_path))
-    meas = drop_unnamed(pd.read_csv(meas_path))
-    cp = drop_unnamed(colpro)
+def numeric_feature_names(colpro):
+    '''model.py's get_feature_names is a better version'''
+    col = drop_unnamed(pd.read_csv(COL_PATH))._get_numeric_data().columns
+    meas = drop_unnamed(pd.read_csv(MEAS_PATH))._get_numeric_data().columns
+    candidates = drop_unnamed(colpro)._get_numeric_data().columns
     iv_list = []
-    for col in cp._get_numeric_data().columns:
-        if col in df._get_numeric_data():
-            iv_list.append(col)
+    for column in candidates:
+        if column in col or column in meas:
+            iv_list.append(column)
     return iv_list
 
-def dummy_out(df):
-    for col in df._get_numeric_data().columns:
-            if df[col].mean() != df[col].fillna(-1).mean():
-                df[col] = df[col].fillna(-1)
-                df[col+'_NA'] = (df[col] == -1)
-    return df
+def dummy_out(data):
+    '''replaces NAs with 0, and makes a dummy varialble if it did'''
+    for col in data._get_numeric_data().columns:
+        if data[col].mean() != data[col].fillna(-1).mean():
+            data[col] = data[col].fillna(-1)
+            data[col+'_NA'] = (data[col] == -1)
+    return data
 
-def median_fill(df):
-    median_features = df._get_numeric_data().dropna().median()
-    return df._get_numeric_data().fillna(median_features)
-
-def drop_unnamed(df):
+def drop_unnamed(data):
+    '''Drops useless columns created by merge'''
     to_drop = []
-    for col in df.columns:
+    for col in data.columns:
         if 'Unnamed:' in col:
             to_drop.append(col)
-    return df.drop(to_drop,1)
-
-def add_dummies(combos):
-    return_cols = combos
-    for x in combos:
-        return_cols.append(x + '_NA')
-    return return_cols
+    return data.drop(to_drop, 1)
 
 def make_colpro():
-    col = pd.read_csv('colData.csv',engine='c')
+    '''Makes the dataset used for analysis by merging precursors'''
+    col = pd.read_csv(COL_PATH, engine='c')
     col = col.sort('year')
-    col = col.drop_duplicates('Name',1)
+    col = col.drop_duplicates('Name', 1)
     meas = read_meas()
-    col_meas = pd.merge(col, meas, left_on ='Name', right_on='name', suffixes=('','_m'))
-    col_meas.to_csv('colmeas.csv')
-    bestrapm = pd.read_csv('bestRapm.csv')
-    colpro = pd.merge(col_meas, bestrapm, left_on='Name',right_on='name', suffixes=('','_p'))
+    col_meas = pd.merge(col, meas, left_on='Name',
+                        right_on='name', suffixes=('', '_m'))
+    col_meas.to_csv('datasets/colmeas.csv')
+    bestrapm = pd.read_csv('datasets/bestRapm.csv')
+    colpro = pd.merge(col_meas, bestrapm, left_on='Name',
+                      right_on='Name', suffixes=('', '_p'))
+
     return drop_unnamed(colpro)
 
-def de_dup(df):
-    df = df.sort('year')
-    df = df.drop_duplicates('Name',1)
+
+def year_fix(year):
+    '''Dummies out year. Not Called at the moment'''
+    year = str(year).replace(' ', '')
+    try:
+        return float(year)
+    except ValueError:
+        print 'ValueError', year
+        return 3000
+
+def read_meas(): #TODO WHY THE VALUE ERRORS
+    '''Fixes year column. May do other things in future'''
+    df = pd.read_csv(MEAS_PATH)
     return df
 
-def year_fix(df):
-    df['year'] = df['year'].str.replace(' ','')
-    years = np.array(df.year)
-    floats = []
-    for x in years:
-        try:
-            floats.append(float(x))
-        except:
-            floats.append(float(-1))
-    df['year'] = floats
-    return df
 
-def read_meas():
-    read_in = pd.read_csv('measurements.csv')
-    df = year_fix(read_in)
-    df = df[df.year.astype('float') > 2000]
-    return df
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 '''Attempts to predict rapm using college stats-Sam Shleifer-August 2nd, 2014'''
 
-from assemble_dataset import lh_vars, dummy_out, median_fill, drop_unnamed
+from assemble_dataset import drop_unnamed
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,55 +12,79 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 
 CLEAN_TRAIN = pd.read_csv('train.csv')
+COL_PATH = 'datasets/colData.csv'
+MEAS_PATH = 'datasets/measurements.csv'
 
-def pipe_attempt(df=CLEAN_TRAIN, dv='tot200'):
-    df = df.fillna(-1)
-    feature_names = get_numeric_features(df)
-    print len(feature_names)
-    target = df[dv]
-    imputer = Imputer(strategy='median', missing_values=-1)
+def rfr(train=CLEAN_TRAIN, target_var='tot200'):
+    '''Best model so far: RandomForestRegressor'''
+    train = train.fillna(-1)
+    feature_names = get_numeric_features(train)
+    X = train[feature_names].values
+    target = train[target_var].values
+    x_train, x_test, y_train, y_test = train_test_split(X, target)
+    imputer = median_imputer(x_train)
+    x_train_imputed = imputer.transform(x_train)
     regressor = RandomForestRegressor(n_estimators=100, max_features=.25)
-    xtr, xt, ytr, yt = train_test_split(df[feature_names].values, df[dv].values)
-    imputer.fit(xtr)
-    xtr_imputed = imputer.transform(xtr)
-    r = regressor.fit(xtr_imputed, np.ravel(ytr))
-    #return df[feature_names], xtr_imputed
-    feature_plot(r, feature_names[1:])
-    #regressor = LinearRegression()
+    regressor.fit(x_train_imputed, np.ravel(y_train))
+    feature_plot(regressor, feature_names[1:])
+    print np.mean(cross_val_score(regressor, X, target, cv=5))
+    return regressor
+
+
+def feature_plot(clf, feature_names): #TODO FEATURE_NAME CROWDING
+    '''Plots feature importances associated with clf'''
+    x_axis = np.arange(len(feature_names))
+    plt.bar(x_axis, clf.feature_importances_)
+    _ = plt.xticks(x_axis + 0.5, feature_names)
+    plt.show()
+
+
+def median_imputer(x_train):
+    '''fits the median imputer'''
+    imputer = Imputer(strategy='median', missing_values=-1)
+    imputer.fit(x_train)
+    return imputer
+
+
+def make_pipeline(train=CLEAN_TRAIN, target_var='tot200'):
+    '''Makes the median imputer -> rfr pipeline'''
+    train = train.fillna(-1)
+    feature_names = get_numeric_features(train)
+    X = train[feature_names].values
+    target = train[target_var].values
+    x_train, x_test, y_train, y_test = train_test_split(X, target)
+    imputer = median_imputer(x_train)
+    regressor = RandomForestRegressor(n_estimators=100, max_features=.25)
+
+    ###GRID SEARCH###
     pipeline = Pipeline([
         ('imp', imputer),
         ('clf', regressor),
     ])
-    scores = cross_val_score(pipeline, df[feature_names].values, target.values, cv=5)
+    return X, target, pipeline
+
+
+def grid_search(info_tuple=None):
+    '''Tests various RFR and imputer params'''
+    if not info_tuple:
+        info_tuple = make_pipeline()
+    X, target, pipeline = info_tuple
+    x_train, x_test, y_train, y_test = train_test_split(X, target)
+    scores = cross_val_score(pipeline, X, target, cv=5)
     print scores.mean()
-    rfr_params = {
-            'imp__strategy' : ['mean', 'median', 'most_frequent'],
-            'clf__max_features' : [0.1, 0.25, 1],
-            'clf__max_depth' : [10, 20, 30]}
-    lin_params = {
-            'imp__strategy' : ['mean', 'median', 'most_frequent'],
-            'clf__normalize': ['True', 'False']}
-    xtr, xt, ytr, yt = four_way_split(df[feature_names], target)
+    rfr_params = {'imp__strategy' : ['mean', 'median', 'most_frequent'],
+                  'clf__max_features' : [0.1, 0.25, 1],
+                  'clf__max_depth' : [10, 20, 30]}
+    lin_params = {'imp__strategy' : ['mean', 'median', 'most_frequent'],
+                  'clf__normalize': ['True', 'False']}
     #gs = GridSearchCV(pipeline, lin_params, cv=5)
     gs = GridSearchCV(pipeline, rfr_params, cv=5)
-    gs.fit(xtr, np.ravel(ytr))
-    return gs
+    gs.fit(x_train, np.ravel(y_train))
+    return gs  #Best = { max_depth: 10, max_features: .25, imp: mean}
 
-#Best = { max_depth: 10, max_features: .25, imp: mean}
 
-def gridded_rfr():
-    rfr = RandomForestRegressor(n_estimators=100, max_features=.25)
-    return rfr
-
-def feature_plot(clf, feature_names):
-    x = np.arange(len(feature_names))
-    plt.bar(x, clf.feature_importances_)
-    _ = plt.xticks(x + 0.5, feature_names)
-    plt.show()
-
-COL_PATH = 'colData.csv'
-MEAS_PATH = 'measurements.csv'
 def get_numeric_features(colpro):
+    '''Gets features that were not created after player was drafted'''
     col = drop_unnamed(pd.read_csv(COL_PATH)).columns
     meas = drop_unnamed(pd.read_csv(MEAS_PATH)).columns
     cp = drop_unnamed(colpro)
@@ -71,15 +95,9 @@ def get_numeric_features(colpro):
             iv_list.append(column)
     return iv_list
 
-def four_way_split(x, y, test_size=.2):
-    xtrain, xtest, ytrain, ytest = train_test_split(x.values, y.values, test_size=test_size)
-    xtrain = pd.DataFrame(xtrain, columns=x.columns)
-    xtest = pd.DataFrame(xtest, columns=x.columns)
-    ytrain = pd.DataFrame(ytrain, columns=['dv'])
-    ytest = pd.DataFrame(ytest, columns=['dv'])
-    return xtrain, xtest, ytrain, ytest
 
 def plot_roc_curve(target_test, target_predicted_proba):
+    '''For classification'''
     fpr, tpr, thresholds = roc_curve(target_test, target_predicted_proba[:, 1])
     roc_auc = auc(fpr, tpr)
     plt.plot(fpr, tpr, label='ROC curve (area = %0.3f)' % roc_auc)
@@ -93,52 +111,29 @@ def plot_roc_curve(target_test, target_predicted_proba):
 
 
 def normalize(x):
-    m = x.mean()
-    s = x.std()
-    return [(a-m)/s for a in x.values]
+    '''Mimics StandardScaler'''
+    mean = x.mean()
+    stdev = x.std()
+    return [(val - mean) / stdev for val  in x.values]
 
 
-def norm_df(df):
-    for col in df._get_numeric_data().columns:
-        df[col] = normalize(df[col])
-    return df
+def norm_df(features):
+    '''normalizes all feature columns'''
+    for col in features:
+        features[col] = normalize(features[col])
+    return features
 
 
-def df_mapper(lh, df):
+def df_mapper(df, features):
+    '''some sklearn_pandas thingy I don't use'''
     to_map = []
-    for col in lh:
+    for col in features:
         df[col] = df[col].astype('float')
         to_map.append((col, StandardScaler()))
     mapper = DataFrameMapper(to_map)
     return mapper
 
-def shuffle(df):
-    return df.loc[np.random.choice(df.index, len(df), replace=False)]
 
-
-
-def rfr_regression(full=CLEAN_TRAIN, dv='tot200', test_size=.7,
-                   add_rfr=True, add_off=True, add_def=True):
-    df = dummy_out(full.copy())
-    xtr, xt, ytr, yt = four_way_split(df[get_numeric_features(df)].values, df[dv].values, test_size=test_size)
-    if add_rfr:
-        rfr = RandomForestRegressor()
-        rfr.fit(np.array(xtr), np.array(ytr[dv].values))
-        xt['forest_pred'] = rfr.predict(np.array(xt.values))
-        xtr['forest_pred'] = rfr.predict(np.array(xtr.values))
-    if add_off:
-        off_reg = skl_reg(xtr, ytr['off100'])
-        xt['off_pred'] = off_reg.predict(np.array(xt.values))
-        xtr['off_pred'] = off_reg.predict(xtr)
-    if add_def:
-        def_reg = skl_reg(xtr, ytr['def100'])
-        xt['def_pred'] = def_reg.predict(np.array(xt.values))
-        xtr['def_pred'] = def_reg.predict(xtr)
-    reg = skl_reg(xtr, ytr[dv])
-    return reg.score(np.array(xt), np.array(yt[dv]))
-
-
-def skl_reg(X, Y):
-    reg = LinearRegression()
-    reg.fit(X, Y)
-    return reg
+def shuffle(data):
+    '''rearranges data, for cross validating by hand'''
+    return data.loc[np.random.choice(data.index, len(data), replace=False)]
