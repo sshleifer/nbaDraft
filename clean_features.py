edad22cf0a32749e230882cbbcfad2326dbdae75
Sam <@kensho.com>
2014-08-05 23:03:00 -0400

    mais limpando

--- clean_features.py
+++ clean_features.py
@@ -5,10 +5,10 @@ def clean_colpro(colpro):
     return order(colpro[column_order], column_order)
 
 column_order = [
-    'Name', u'Team','pick', 'age', 'pos', u'GP', u'Min',
+    'year', 'Name', u'Team','pick', 'age', 'pos', u'GP', u'Min',
     u'Pts', u'FG', u'FGA', u'FG%', u'2Pt', u'2PtA', u'2P%', u'3Pt',
     u'3PtA', u'3P%', u'FTM', u'FTA', u'FT%', u'Off', u'Def', u'TOT',
-    u'Asts', u'Stls', u'Blks', u'TOs', u'PFs', u'year', u'PTs/g',
+    u'Asts', u'Stls', u'Blks', u'TOs', u'PFs', u'PTs/g',
     u'FGA/g',u'Pts/Play', u'TS%', u'eFG%', u'FTA/FGA', u'3PA/FGA',
     u'Ast/g',u'Ast/FGA', u'A/TO', u'PPR', u'BK/g', u'STL/g', u'PF/g',
     u'Weight', u'Reach', u'Body Fat',u'Hand Length', u'Hand Width',
